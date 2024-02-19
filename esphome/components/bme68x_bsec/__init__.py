1f83603fa104d66049b07872a1f93bf1602dc211
Rui Marinho <@uphold.com>
2024-02-19 22:04:30 +0000

    Update BSEC2 to v1.7.2502

--- esphome/components/bme68x_bsec/__init__.py
+++ esphome/components/bme68x_bsec/__init__.py
@@ -75,5 +75,5 @@ async def to_code(config):
     cg.add_library(
         "BSEC2 Software Library",
         None,
-        "https://github.com/boschsensortec/Bosch-BSEC2-Library.git#v1.6.2400",
+        "https://github.com/boschsensortec/Bosch-BSEC2-Library.git#v1.7.2502",
     )
